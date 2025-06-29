3721ea57050442b4c409a50808aac2fc0a7975f8
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2025-06-28 22:02:43 -0300

    Try to fix sqlite3

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# Correção para o SQLite no Streamlit Cloud
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import google.generativeai as genai
 import os
